15e18cf1eb406b8c672006bef9619c1a1ffadac8
heozzi <hz0819@naver.com>
2025-02-24 17:56:59 +0900

    CI/CD_Test3

--- src/test/java/com/example/redisdbtest/RedisDbTestApplicationTests.java
+++ src/test/java/com/example/redisdbtest/RedisDbTestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RedisDbTestApplicationTests extends RedisDbTestApplication {
+class RedisDbTestApplicationTests {
 
     @Test
     void contextLoads() {
